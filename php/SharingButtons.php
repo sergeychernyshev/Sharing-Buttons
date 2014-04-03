a954cf9baf28008dd46106a47c1b26783eea0483
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2014-04-02 22:49:24 -0400

    Fixed PHP formatting

--- php/SharingButtons.php
+++ php/SharingButtons.php
@@ -1,9 +1,10 @@
-<?
+<?php
+
 /**
- * SharingButtons class 
+ * SharingButtons class
  */
-class SharingButtons 
-{
+class SharingButtons {
+
 	public static $VariableModifiers = array();
 
 	/**
@@ -11,48 +12,42 @@ class SharingButtons
 	 * aaa portion becomes variable name
 	 * bbb,ccc and other consequent portions separated by "." are variable modifiers and ignored
 	 */
-	public static function extractVariablesFromString($string)
-	{
+	public static function extractVariablesFromString($string) {
 		$names = array();
 
 		preg_match_all('/\{\$(?<name>[\w\d-]+)(\..*?)?\}/', $string, $matches);
 
 		/**
-		* We only need unique names
-		*/
-		foreach ($matches['name'] as $var_name)
-		{
+		 * We only need unique names
+		 */
+		foreach ($matches['name'] as $var_name) {
 			$names[$var_name] = true;
 		}
 
 		return array_keys($names);
 	}
 
-	public static function substituteVariablesInString($string, $variables, $default_modifier = 'raw')
-	{
+	public static function substituteVariablesInString($string, $variables, $default_modifier = 'raw') {
 		/*
-		* If default modifier with name specified doesn't exist, use "raw" instead
-		*/
-		if (!array_key_exists($default_modifier, self::$VariableModifiers))
-		{
+		 * If default modifier with name specified doesn't exist, use "raw" instead
+		 */
+		if (!array_key_exists($default_modifier, self::$VariableModifiers)) {
 			$default_modifier = 'raw';
 		}
 
 		/*
-		* Finding all variables used in a string and their modifiers
-		*/
+		 * Finding all variables used in a string and their modifiers
+		 */
 		preg_match_all('/\{\$(?<name>[\w\d-]+)(?<modifiers>\.[\.\w\d-]+)?\}/', $string, $matches);
 
 		$varcalls = array(); // variable calls in template
 		$replacements = array(); // corresponding replacements
-		foreach ($matches['name'] as $varname)
-		{
+		foreach ($matches['name'] as $varname) {
 			$varcall = array_shift($matches[0]);
 			$varcalls[] = $varcall;
 
 			// if this variable wasn't specified, replace with empty string
-			if (!array_key_exists($varname, $variables))
-			{
+			if (!array_key_exists($varname, $variables)) {
 				$replacements[] = '';
 				array_shift($matches['modifiers']);
 				continue;
@@ -65,18 +60,16 @@ class SharingButtons
 
 			// Modifying with each modifier in the row (there might be multiple modifiers)
 			$modifiers = $modifiers_string !== '' ?
-				split('\.', $modifiers_string) : array($default_modifier);
+					split('\.', $modifiers_string) : array($default_modifier);
 
-			foreach ($modifiers as $modifier)
-			{
-				if ($modifier == 'raw' || $modifier == '')
-				{
+			foreach ($modifiers as $modifier) {
+				if ($modifier == 'raw' || $modifier == '') {
 					continue;
 				}
 
 				if (array_key_exists(strtolower($modifier), self::$VariableModifiers)) {
 					$replacement = self::$VariableModifiers[strtolower($modifier)]
-						->modify($replacement);
+							->modify($replacement);
 				}
 			}
 			$replacements[] = $replacement;
@@ -84,20 +77,16 @@ class SharingButtons
 		return str_replace($varcalls, $replacements, $string);
 	}
 
-	public static function areAllVariablesDefined($strings, $variables)
-	{
-		foreach ($strings as $string)
-		{
+	public static function areAllVariablesDefined($strings, $variables) {
+		foreach ($strings as $string) {
 			/*
-			* Finding all variables used in a string
-			*/
+			 * Finding all variables used in a string
+			 */
 			preg_match_all('/\{\$(?<name>[\w\d-]+)(\.[\.\w\d-]+)?\}/', $string, $matches);
 
-			foreach ($matches['name'] as $varname)
-			{
+			foreach ($matches['name'] as $varname) {
 				// if this variable wasn't specified, replace with empty string
-				if (!array_key_exists($varname, $variables) || $variables[$varname] == '')
-				{
+				if (!array_key_exists($varname, $variables) || $variables[$varname] == '') {
 					return false;
 				}
 			}
@@ -105,46 +94,48 @@ class SharingButtons
 
 		return true;
 	}
+
 }
 
 /*
  * Variable modifiers
  */
-abstract class VariableModifier
-{
+
+abstract class VariableModifier {
+
 	public abstract function modify($value);
 }
 
-class UrlEncoder_VariableModifier extends VariableModifier
-{
-	public function modify($value)
-	{
+class UrlEncoder_VariableModifier extends VariableModifier {
+
+	public function modify($value) {
 		return urlencode($value);
 	}
-}
- 
-class RemoveURLProtocol_VariableModifier extends VariableModifier
-{
-	public function modify($value)
-	{
-		return preg_replace('|^http://|', '', $value);
-	}
-}
- 
-class UrlSpaceEncoder_VariableModifier extends VariableModifier
-{
-	public function modify($value)
-	{
-		return preg_replace('/\+/', '%20', urlencode($value));
-	}
+
 }
 
-class UrlUnderscoreEncoder_VariableModifier extends VariableModifier
-{
-	public function modify($value)
-	{
+class RemoveURLProtocol_VariableModifier extends VariableModifier {
+
+	public function modify($value) {
+		return preg_replace('|^http://|', '', $value);
+	}
+
+}
+
+class UrlSpaceEncoder_VariableModifier extends VariableModifier {
+
+	public function modify($value) {
+		return preg_replace('/\+/', '%20', urlencode($value));
+	}
+
+}
+
+class UrlUnderscoreEncoder_VariableModifier extends VariableModifier {
+
+	public function modify($value) {
 		return preg_replace('/\+/', '_', urlencode($value));
 	}
+
 }
 
 // static members can't be populated with objects so we need to set them separately
@@ -153,4 +144,4 @@ SharingButtons::$VariableModifiers = array(
 	'spaceencoded' => new UrlSpaceEncoder_VariableModifier(),
 	'underscoreencoded' => new UrlUnderscoreEncoder_VariableModifier(),
 	'noprotocol' => new RemoveURLProtocol_VariableModifier()
-); 
+);
